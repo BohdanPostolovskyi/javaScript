9808e1c81604bfffb87e573eee8ba949bddc264b
Postolovskyi Yarema <91177682+BohdanPostolovskyi@users.noreply.github.com>
2024-05-01 01:06:59 +0300

    Update script.js

--- Lab6/js/script.js
+++ Lab6/js/script.js
@@ -7,6 +7,9 @@ let currentSteps = 0;
 let timer;
 let isGameWon = false;
 
+let isRestart = true;
+let currentMatrix;
+
 window.onload = function() {
     loadGame();
 }
@@ -135,16 +138,20 @@ function restartGame() {
 }
 
 function getRandomJsonFile() {
-    const jsonFiles = ['JSONs/matrix_1.json', 'JSONs/matrix_2.json', 'JSONs/matrix_3.json', 'JSONs/matrix_4.json'];
-    const randomIndex = Math.floor(Math.random() * jsonFiles.length);
-    return jsonFiles[randomIndex];
+    if (isRestart) {
+        const jsonFiles = ['JSONs/matrix_1.json', 'JSONs/matrix_2.json', 'JSONs/matrix_3.json', 'JSONs/matrix_4.json'];
+        const randomIndex = Math.floor(Math.random() * jsonFiles.length);
+        currentMatrix = jsonFiles[randomIndex];
+        isRestart = false;
+    }
 }
 
 function loadNewGame() {
+    isRestart = true;
     currentSteps = 0;
     isGameWon = false;
     clearInterval(timer);
-    loadGame(getRandomJsonFile());
+    loadGame();
 }
 
 function startTimer() {
