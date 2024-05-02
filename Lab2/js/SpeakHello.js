18df71baf64cc7142b0963a1b29120b06dd5a54a
Postolovskyi Yarema <91177682+BohdanPostolovskyi@users.noreply.github.com>
2024-05-02 23:00:27 +0300

    Add files via upload

---
+++ Lab2/js/SpeakHello.js
@@ -0,0 +1,11 @@
+(function(window) {
+    let speakHello = {};
+    speakHello.speakWord = "Hello";
+
+    speakHello.speak = function (name) {
+        console.log(speakHello.speakWord + " " + name);
+    }
+
+    window.speakHello = speakHello;
+
+  })(window);
